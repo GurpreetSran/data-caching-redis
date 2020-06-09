1b68eac5bf4ae2e75b8b2961a8fc948bf3823cf4
Gurpreet Singh <gurpreet.singh@preqin.com>
2020-06-09 09:56:47 +0100

    update packages

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '927304560273-vv1mqhb3hmtnfo6je2e6g213kfc456qs.apps.googleusercontent.com',
+  googleClientSecret: 'aRoIbJTzWW5-baVuXAlMT-9u',
+  mongoURI:
+    'mongodb+srv://gsran:itYJGDQbIjSMsYbF@cluster0-bmnmr.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
