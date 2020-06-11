a32a47ff57de6627b966c7eb264fe6bc54f85dc1
Gurpreet Singh <gurpreet.singh@preqin.com>
2020-06-11 16:00:53 +0100

    .

--- config/ci.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '927304560273-vv1mqhb3hmtnfo6je2e6g213kfc456qs.apps.googleusercontent.com',
-  googleClientSecret: 'aRoIbJTzWW5-baVuXAlMT-9u',
-  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379',
-};
